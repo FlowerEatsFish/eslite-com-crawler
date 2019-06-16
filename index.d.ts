952ac176eaa37826397d5f3be900ae04e491ae79
Eating-Fish Flower <flower.eating.fish@gmail.com>
2019-06-16 17:15:50 +0800

    Add unit test using Jest

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+export interface PriceField {
+  discount: number | null;
+  currentPrice: number | null;
+}
+
+export interface DetailType {
+  title: string;
+  url: string;
+  author: string[] | null;
+  publisher: string | null;
+  publicationDate: string | null;
+  imageUrl: string | null;
+  price: PriceField;
+  introduction: string | null;
+}
+
+export type EsliteComCollectionFunction =
+  (keyword: string, page?: number) => DetailType[] | null;
+
+declare const esliteComCollectionApi: EsliteComCollectionFunction;
+
+export default esliteComCollectionApi;
