0fd813c5c1cd00ffdb26d4d0ede00de723f35b39
Eating-Fish Flower <flower.eating.fish@gmail.com>
2021-09-05 22:17:02 +0800

    Test: fetch data from mocked ones

--- test/index.ts
+++ test/index.ts
@@ -1,3 +1,10 @@
+import axios from "axios";
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+import emptyData from "./mocks/empty-data.json";
+import multipleData from "./mocks/multiple-data.json";
+
 import { DetailType } from "../index";
 import esliteComCollection from "../src/index";
 
@@ -7,8 +14,9 @@ describe("Run demo", (): void => {
   it(
     "Should get results as Array",
     async (): Promise<void> => {
-      const result = await esliteComCollection("文學少女");
+      mockedAxios.get.mockResolvedValue({ status: 200, data: multipleData });
 
+      const result = await esliteComCollection("文學少女");
       expect(Array.isArray(result)).toBeTruthy();
       expect((result as DetailType[]).length).toBeGreaterThan(0);
     },
@@ -18,8 +26,9 @@ describe("Run demo", (): void => {
   it(
     "Should do not have any result as Null",
     async (): Promise<void> => {
-      const result = await esliteComCollection("blablablablablablablablablablablabla");
+      mockedAxios.get.mockResolvedValue({ status: 200, data: emptyData });
 
+      const result = await esliteComCollection("blablablablablablablablablablablabla");
       expect(result).toBeNull();
     },
     timeout,
